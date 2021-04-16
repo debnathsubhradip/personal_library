6bb67b881960e85c6bdd41a665743e06cfc2e846
debnath.subhradip <debnath.subhradip@gmail.com>
2021-04-16 18:52:35 +0530

    small changes made

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,9 @@
 function casualread(){
-    window.open('casualread.html');
+    window.open('casualread.html',"_self");
 }
 function engineerread(){
-    window.open('engineerread.html');
+    window.open('engineerread.html',"_self");
 }
 function home(){
-    window.open('index.html');
+    window.open('index.html',"_self");
 }
